95744d6381e393211848dfb9dacb03fe7f111ba3
aknysh <aknysh>
2017-10-28 14:08:07 -0400

    Fix `outputs.tf`

--- outputs.tf
+++ outputs.tf
@@ -5,19 +5,20 @@ output "ngw_id" {
 
 output "ngw_private_ip" {
   value       = "${join("", aws_nat_gateway.default.*.private_ip)}"
-  description = "Private IP addresse of the NAT Gateway"
+  description = "Private IP address of the NAT Gateway"
 }
 
 output "ngw_public_ip" {
   value       = "${join("", aws_nat_gateway.default.*.public_ip)}"
-  description = "Public IP addresse of the NAT Gateway"
+  description = "Public IP address of the NAT Gateway"
 }
 
 output "subnet_ids" {
   value       = ["${coalescelist(aws_subnet.private.*.id, aws_subnet.public.*.id)}"]
-  description = "IDs of private subnets"
+  description = "Subnets IDs"
 }
 
 output "route_table_ids" {
-  value = ["${coalescelist(aws_route_table.public.*.id, aws_route_table.private.*.id)}"]
+  value       = ["${coalescelist(aws_route_table.public.*.id, aws_route_table.private.*.id)}"]
+  description = "Route Table IDs"
 }
