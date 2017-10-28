11173e758138f1ea36ab3832e18341ea71b00080
aknysh <aknysh>
2017-10-27 23:46:27 -0400

    Add `tags` to `aws_nat_gateway`

--- public.tf
+++ public.tf
@@ -67,6 +67,7 @@ resource "aws_nat_gateway" "default" {
   count         = "${var.type == "public" ? 1 : 0}"
   allocation_id = "${join("", aws_eip.default.*.id)}"
   subnet_id     = "${element(aws_subnet.public.*.id, 0)}"
+  tags          = "${module.public_label.tags}"
 
   lifecycle {
     create_before_destroy = true
