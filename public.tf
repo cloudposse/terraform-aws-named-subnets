9032246b06f0b55687e1c26627eb2e4fde9cf4c1
SweetOps <vladimir.fess@gmail.com>
2017-11-16 14:53:31 +0200

    coventions =\

--- public.tf
+++ public.tf
@@ -1,5 +1,6 @@
 locals {
   public_count = "${var.enabled == "true" && var.type == "public" ? length(var.subnet_names) : 0}"
+  ngw_count    = "${var.enabled == "true" && var.type == "public" && var.nat_enabled == "true" ? 1 : 0}"
 }
 
 module "public_label" {
@@ -59,4 +60,24 @@ resource "aws_network_acl" "public" {
   egress     = "${var.public_network_acl_egress}"
   ingress    = "${var.public_network_acl_ingress}"
   tags       = "${module.public_label.tags}"
+}
+
+resource "aws_eip" "default" {
+  count = "${local.ngw_count}"
+  vpc   = "true"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_nat_gateway" "default" {
+  count         = "${local.ngw_count}"
+  allocation_id = "${join("", aws_eip.default.*.id)}"
+  subnet_id     = "${element(aws_subnet.public.*.id, 0)}"
+  tags          = "${module.public_label.tags}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
