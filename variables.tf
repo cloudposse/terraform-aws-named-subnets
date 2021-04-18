0e43e02a887bc0c3560a1f429f1e99713be64bcb
Marcin Brański <marcin.branski@toptal.com>
2021-04-18 08:27:29 +0200

    Fix nat_gateway_id network_interface_id variable defaults conflict

--- variables.tf
+++ variables.tf
@@ -33,13 +33,13 @@ variable "cidr_block" {
 variable "igw_id" {
   type        = string
   description = "Internet Gateway ID which will be used as a default route in public route tables (e.g. `igw-9c26a123`). Conflicts with `ngw_id`"
-  default     = ""
+  default     = null
 }
 
 variable "ngw_id" {
   type        = string
   description = "NAT Gateway ID which will be used as a default route in private route tables (e.g. `igw-9c26a123`). Conflicts with `igw_id`"
-  default     = ""
+  default     = null
 }
 
 variable "public_network_acl_id" {
@@ -158,7 +158,7 @@ variable "nat_enabled" {
 
 variable "eni_id" {
   type        = string
-  default     = ""
+  default     = null
   description = "An ID of a network interface which is used as a default route in private route tables (_e.g._ `eni-9c26a123`)"
 }
 
